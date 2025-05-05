71c2042cf9a9a042650513e3906a9472f81a4f0b
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-05-05 20:37:04 +0200

    Create products finished

--- products/views/products_views.py
+++ products/views/products_views.py
@@ -1,7 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import redirect, render, get_object_or_404
 
-from ..models import Product
+from ..models import Product, Ingredient
 
 
 def products_list(request):
@@ -23,13 +23,22 @@ def products_add(request):
             base_price = request.POST.get('base_price') or 1,
             fixed_costs = request.POST.get('fixed_costs') or 1,
             price_mod = request.POST.get('price_mod') or 1,
-            final_price = request.POST.get('final_price') or 1,
-
-            ingredients = request.POST.getlist('ingredients'),
-            ingredients_from_products = request.POST.getlist('ingredients_from_products')
+            final_price = request.POST.get('final_price') or 1
         )
+
+        product.ingredients.set(request.POST.getlist('ingredients'))
+        product.ingredients_from_products.set(request.POST.getlist('ingredients_from_products'))
+
         product.save()
 
         return redirect('products:home')
 
-    return render(request, 'products/product-form.html')
+    ingredients = Ingredient.objects.all()
+    ingredients_from_products = Product.objects.all()
+
+    return render(request,
+                  'products/product-form.html',
+                  {
+                      "ingredients_list": ingredients,
+                      "ingredients_from_products_list": ingredients_from_products
+                })
