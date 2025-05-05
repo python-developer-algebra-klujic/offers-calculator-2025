19654f6ac9724e7696c21a4f3415294aa87f0e5e
klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-05-05 18:14:25 +0200

    Update urls.py

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('ingredients/', views.ingredients_list, name='ingredients_list'),
-    path('ingredients/db-seed/', views.ingredients_db_seed, name='db_seed'),
+    path('ingredients/db-seed/', views.ingredients_seeder, name='db_seed'),
     path('ingredients/add/', views.ingredient_add, name='ingredient_add'),
     path('ingredients/edit/<int:pk>', views.ingredient_edit, name='ingredient_edit'),
     path('ingredients/<int:pk>', views.ingredient_details, name='ingredient_details'),
